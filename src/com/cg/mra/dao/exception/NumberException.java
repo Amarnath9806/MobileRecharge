764b918d5a1bd6d5518b1ceeceeacf427d99843e
Amarnath Kanapuram <amarnath9806@gmail.com>
2019-03-08 17:54:40 +0530

    new

---
+++ src/com/cg/mra/dao/exception/NumberException.java
@@ -0,0 +1,8 @@
+package com.cg.mra.dao.exception;
+
+public class NumberException extends Exception {
+
+	public NumberException() {
+		System.out.println("mobile number should be 10 digit");
+	}
+}
